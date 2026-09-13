0d85b1dfafe83b85a78d8c733db6f87396a32860
CoumbaThiep <diopcoumbathiep@gmail.com>
2026-09-13 02:38:40 +0000

    feat: ajouter les enums du domaine

---
+++ src/main/java/sn/woy/reservation/domain/TypeSalle.java
@@ -0,0 +1,10 @@
+package sn.woy.reservation.domain;
+
+/** Type d'usage prévu pour une salle (ensemble fermé de valeurs). */
+public enum TypeSalle {
+    SALLE_COURS,
+    SALLE_TP,
+    SALLE_REUNION,
+    AMPHITHEATRE,
+    SALLE_SOUTENANCE
+}
